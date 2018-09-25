77f1d03b6e438ba5a0f3d05a431758b56deead6c
ybc9977 <ybc9977@gmail.com>
2018-09-25 16:43:01 +0800

    25/09/18

--- gtcs.sh
+++ gtcs.sh
@@ -50,13 +50,7 @@ ssh -o StrictHostKeyChecking=no -i ~/.ssh/gtcs.pem ${line} "sh ~/alluxio-gtcs/ma
 
 sleep 40
 
-i=1
-while read -r line
-do
-    ssh -o StrictHostKeyChecking=no -i ~/.ssh/gtcs.pem ${line} "cd alluxio-gtcs; zip -r $i.zip ./logs" < /dev/null
-    scp -o StrictHostKeyChecking=no -i ~/.ssh/gtcs.pem -r ${line}:~/alluxio-gtcs/$i.zip ~/Downloads/
-    let i+=1
-done < ~/flintrock.txt
-
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/gtcs.pem ${line} "cd alluxio-gtcs; zip -r 1.zip ./logs" < /dev/null
+scp -o StrictHostKeyChecking=no -i ~/.ssh/gtcs.pem -r ${line}:~/alluxio-gtcs/1.zip ~/Downloads/
 
 exit 0
