c89542c03ffe7482aa8c50d307e867197fb815db
ybc9977 <ybc9977@gmail.com>
2018-09-25 19:14:29 +0800

    25/09/18

--- master.sh
+++ master.sh
@@ -4,7 +4,7 @@ mkdir ~/testFile
 touch ~/testFile/1.txt
 echo "asodhfasiuhgilsjad" > ~/testFile/1.txt
 
-for ((i=2; i<30; i ++))
+for ((i=2; i<=30; i ++))
 do
     cp ~/testFile/1.txt ~/testFile/$i.txt
 done
