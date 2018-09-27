f7610e780159d740a986731e97c890c8f333bd2c
ybc9977 <ybc9977@gmail.com>
2018-09-27 12:07:43 +0800

    27/09/18

--- stop.sh
+++ stop.sh
@@ -5,5 +5,5 @@
 python3 ~/Downloads/flintrock-master/standalone.py run-command gtcs "~/alluxio-gtcs/bin/alluxio-stop.sh all"
 
 # clear logs/UnderFileStorage
-python3 ~/Downloads/flintrock-master/standalone.py run-command gtcs "rm ~/alluxio-gtcs/logs;rm ~/alluxio-gtcs/underFSStorage/*.txt"
+python3 ~/Downloads/flintrock-master/standalone.py run-command gtcs "rm -rf ~/alluxio-gtcs/logs ;rm -rf ~/alluxio-gtcs/underFSStorage/;mkdir ~/alluxio-gtcs/underFSStorage"
 
