dcbe6c1001462305da3d5be4c37a8c1d9c327495
ybc9977 <ybc9977@gmail.com>
2018-10-04 21:12:54 +0800

    04/10/18

--- script/start.sh
+++ script/start.sh
@@ -6,7 +6,7 @@ i=1
 while read -r line
 do
     test $i -lt 2 && let "i++" && continue
-    test $i -gt 1+$1 && let "i++" && continue
+    test $i -gt $[1+$1] && let "i++" && continue
     python3 ~/Downloads/flintrock-master/standalone.py run-command gtcs "echo '${line}' >> ~/alluxio-gtcs/conf/workers"
     let "i++"
 done < ~/flintrock.txt
@@ -17,7 +17,7 @@ ssh -o StrictHostKeyChecking=no -i ~/.ssh/gtcs.pem ${line} "~/alluxio-gtcs/bin/a
 i=1
 while read -r line
 do
-    test $i -le 1+$1 && let "i++" && continue
+    test $i -le $[1+$1] && let "i++" && continue
     ssh -o StrictHostKeyChecking=no -i .ssh/gtcs.pem ${line} "~/alluxio-gtcs/bin/alluxio-start.sh client"
     let "i++"
 done < ~/flintrock.txt
@@ -25,6 +25,6 @@ done < ~/flintrock.txt
 sleep 30
 
 read -r line < ~/flintrock.txt
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/gtcs.pem ${line} "for ((i=1; i<=500; i ++)); do ~/alluxio-gtcs/bin/alluxio fs copyFromLocal ~/testFile/\$i.txt /test; done;"
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/gtcs.pem ${line} "for ((i=1; i<=500; i ++)); do ~/alluxio-gtcs/bin/alluxio fs copyFromLocal ~/testFile/\$i.txt /test/; done;"
 
 exit 0
