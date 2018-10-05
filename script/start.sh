f7c8e4e67cb71638d8b44636970c3c43d8d1ebf7
ybc9977 <ybc9977@gmail.com>
2018-10-05 21:53:07 +0800

    05/10/18

--- script/start.sh
+++ script/start.sh
@@ -1,39 +1,39 @@
-#!/bin/bash
+# #!/bin/bash
 
-# start
-
-i=1
-while read -r line
-do
-    test $i -lt 2 && let "i++" && continue
-    test $i -gt $[1+$1] && let "i++" && continue
-    python3 ~/Downloads/flintrock-master/standalone.py run-command gtcs "echo '${line:9}' > ~/alluxio-gtcs/conf/workers"
-    let "i++"
-done < ~/flintrock.txt
-
-read -r line < ~/flintrock.txt
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/gtcs.pem ${line} " ~/Hadoop/sbin/start-dfs;~/alluxio-gtcs/bin/alluxio-start.sh all"
+# # start
 
 # i=1
 # while read -r line
 # do
-#     test $i -le 1 && let "i++" && continue
+#     test $i -lt 2 && let "i++" && continue
 #     test $i -gt $[1+$1] && let "i++" && continue
-#     ssh -o StrictHostKeyChecking=no -i .ssh/gtcs.pem ${line} "~/alluxio-gtcs/bin/alluxio-start.sh worker"
+#     python3 ~/Downloads/flintrock-master/standalone.py run-command gtcs "echo '${line:9}' > ~/alluxio-gtcs/conf/workers"
 #     let "i++"
 # done < ~/flintrock.txt
 
-i=1
-while read -r line
-do
-    test $i -le $[1+$1] && let "i++" && continue
-    ssh -o StrictHostKeyChecking=no -i .ssh/gtcs.pem ${line} "~/alluxio-gtcs/bin/alluxio-start.sh client"
-    let "i++"
-done < ~/flintrock.txt
+# read -r line < ~/flintrock.txt
+# ssh -o StrictHostKeyChecking=no -i ~/.ssh/gtcs.pem ${line} " ~/Hadoop/sbin/start-dfs;~/alluxio-gtcs/bin/alluxio-start.sh all"
 
-sleep 30
+# # i=1
+# # while read -r line
+# # do
+# #     test $i -le 1 && let "i++" && continue
+# #     test $i -gt $[1+$1] && let "i++" && continue
+# #     ssh -o StrictHostKeyChecking=no -i .ssh/gtcs.pem ${line} "~/alluxio-gtcs/bin/alluxio-start.sh worker"
+# #     let "i++"
+# # done < ~/flintrock.txt
+
+# i=1
+# while read -r line
+# do
+#     test $i -le $[1+$1] && let "i++" && continue
+#     ssh -o StrictHostKeyChecking=no -i .ssh/gtcs.pem ${line} "~/alluxio-gtcs/bin/alluxio-start.sh client"
+#     let "i++"
+# done < ~/flintrock.txt
+
+# sleep 30
 
 read -r line < ~/flintrock.txt
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/gtcs.pem ${line} "sh ~/alluxio-gtcs/file_copy.sh"
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/gtcs.pem ${line} "sh ~/alluxio-gtcs/script/file_copy.sh"
 
 exit 0
