641fb7eede159883c7ceba410e26daeefda4a3f9
ybc9977 <ybc9977@gmail.com>
2018-10-05 15:26:30 +0800

    04/10/18

--- script/add_slave.sh
+++ script/add_slave.sh
@@ -2,7 +2,7 @@
 
 # $1 is the number of workers while $2 is the number of clients
 
-python3 ~/Downloads/flintrock-master/standalone.py add-slaves gtcs --num-slaves $[$1+$2]
+python3 ~/Downloads/flintrock-master/standalone.py add-slaves gtcs --num-slaves $1
 
 python3 ~/Downloads/flintrock-master/standalone.py describe gtcs
 
