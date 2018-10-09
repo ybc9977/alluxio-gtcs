95802350dcd1c37a4748dd7067c0fe2e7d81418a
ybc9977 <ybc9977@gmail.com>
2018-10-09 23:12:45 +0800

    09/10/18

--- script/file_copy.sh
+++ script/file_copy.sh
@@ -2,5 +2,5 @@
 
 for ((i=1; i<=500; i ++)); 
 do 
-~/alluxio-gtcs/bin/alluxio fs copyFromLocal ~/testFile/$i.txt /test/$i.txt
+~/alluxio-gtcs/bin/alluxio fs copyFromLocal ~/testFile/1.txt /test/$i.txt
 done;
--- script/file_preparation.sh
+++ script/file_preparation.sh
@@ -6,7 +6,7 @@ cd ~/testFile
 
 dd if=/dev/zero of=1.txt bs=104857600 count=1
 
-for ((i=2; i<=500; i ++))
-do
-    cp ~/testFile/1.txt ~/testFile/$i.txt
-done
+# for ((i=2; i<=500; i ++))
+# do
+#     cp ~/testFile/1.txt ~/testFile/$i.txt
+# done
