2ea6f80ba4588980d4eb53d54272cf5e6e953a7f
ybc9977 <ybc9977@gmail.com>
2019-01-18 12:58:05 +0800

    updateGame

---
+++ script/updatePref.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+~/alluxio-gtcs/bin/alluxio updatePrefComp 500 300 2 7
+~/alluxio-gtcs/bin/alluxio updatePrefComp 500 300 6 7
+~/alluxio-gtcs/bin/alluxio updatePrefComp 500 300 10 7
+~/alluxio-gtcs/bin/alluxio updatePrefComp 500 300 14 7
+~/alluxio-gtcs/bin/alluxio updatePrefComp 500 300 20 7
